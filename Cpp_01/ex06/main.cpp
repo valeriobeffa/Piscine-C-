242084b7a2068ce886566af2a615ed49eaf27a61
valeriobeffa <70881989+valeriobeffa@users.noreply.github.com>
2021-11-19 16:33:09 +0100

    Update main.cpp

--- Cpp_01/ex06/main.cpp
+++ Cpp_01/ex06/main.cpp
@@ -5,6 +5,12 @@ int main(int argc, char **argv)
     Karen karen;
     if (argc == 2)
     {
-        karen.complain(argv[1]);
+        std::string string = argv[1];
+        if (!string.compare("DEBUG") || !string.compare("INFO") || !string.compare("WARNING") || !string.compare("ERROR"))
+            karen.complain(argv[1]);
+        else
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
     }
+    else
+        std::cout << "Wrong arguments" << std::endl;
 }
